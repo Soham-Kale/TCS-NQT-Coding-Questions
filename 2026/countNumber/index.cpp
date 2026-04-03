3d659b942e889eec69273561cf249dfd6242adf7
Soham-Kale <sohamkale3014@gmail.com>
2026-04-03 22:02:02 +0530

    basic

---
+++ 2026/countNumber/index.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int countNumbers(vector<int>&s, int n) {
+    int count = 1;
+    for(int i=0; i<s.size(); i++) {
+        if(s[i] >= n) {
+            count ++;
+            i++;
+        } else {
+            i++;
+        }
+    }
+    return count;
+}
+
+int main() {
+    int n;
+    cin >> n;
+    
+    vector<int>s(n);
+    for(int i=0; i<n; i++) {
+        cin >> s[i];
+    }
+    
+    int ans = countNumbers(s, n);
+    cout << ans;
+    
+    return 0;
+}
