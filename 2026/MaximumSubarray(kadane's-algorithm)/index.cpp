4642cad63277ac109d71fef4f2df9b2326209850
Soham-Kale <sohamkale3014@gmail.com>
2026-04-07 15:29:52 +0530

    Refactor Maximum Subarray implementation and add input handling for user-defined arrays

--- 2026/MaximumSubarray(kadane's-algorithm)/index.cpp
+++ 2026/MaximumSubarray(kadane's-algorithm)/index.cpp
@@ -1,3 +1,6 @@
+#include <bits/stdc++.h>
+using namespace std;
+
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
@@ -6,7 +9,7 @@ public:
 
         int start = 0, end = 0, temp = 0;
 
-        for(int i=1; i<nums.size(); i++) {
+        for(int i = 1; i < nums.size(); i++) {
             if(currentSum + nums[i] < nums[i]) {
                 currentSum = nums[i];
                 temp = i;
@@ -20,6 +23,37 @@ public:
                 end = i;
             }
         }
+
+        cout << "Subarray index range: " << start << " to " << end << endl;
         return maxSum;
     }
 };
+
+int main() {
+    cout << "Enter array (space or comma separated): ";
+
+    string line;
+    getline(cin, line);
+
+    vector<int> nums;
+    string temp = "";
+
+    for(char c : line) {
+        if(c == ',' || c == ' ') {
+            if(!temp.empty()) {
+                nums.push_back(stoi(temp));
+                temp = "";
+            }
+        } else {
+            temp += c;
+        }
+    }
+    if(!temp.empty()) nums.push_back(stoi(temp));
+
+    Solution obj;
+    int result = obj.maxSubArray(nums);
+
+    cout << "Maximum Subarray Sum: " << result << endl;
+
+    return 0;
+}
