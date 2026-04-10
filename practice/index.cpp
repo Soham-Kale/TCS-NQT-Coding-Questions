d3d93817ea239f1a3b1934a7e825f9e6abbf2a0e
Soham-Kale <sohamkale3014@gmail.com>
2026-04-10 22:04:26 +0530

    Refactor input handling: replace string input with dynamic array size and implement array printing

--- practice/index.cpp
+++ practice/index.cpp
@@ -1,11 +1,28 @@
+/// Using Dynamic Array (vector in C++)
+
 #include<bits/stdc++.h>
 using namespace std;
 
 int main() {
-    string s;
-    cin >> s
+    int n;
+    cout << "Enter the size of the array: ";
+    cin >> n;
 
-    geltline(cin, s);
+    vector<int> list;
 
-    
+    for(int i=0; i<n; i++) {
+        int x;
+        cin >> x;
+        list.push_back(x); 
+    }
+
+    // print array
+    for(int i=0; i<n; i++) {
+        cout << list[i] << " ";
+    }
+
+    // another way to print array using for each loop
+    for(int x: list) {
+        cout << x << " ";
+    }
 }
