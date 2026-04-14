a181e8e1bfa0ee5040e8316b93e7bb5e7a9faaa9
Soham-Kale <sohamkale3014@gmail.com>
2026-04-14 23:02:45 +0530

    Refactor input method and enhance printSum function in 2D Array example

--- 2D-Arrays/basic/index.cpp
+++ 2D-Arrays/basic/index.cpp
@@ -2,6 +2,7 @@
 using namespace std;
 
 bool isPresent(int arr[][4], int target, int row, int col) {
+
     for(int row=0; row<3; row++) {
         for(int col=0; col<4; col++) {
             if(arr[row][col] == target) {
@@ -12,18 +13,19 @@ bool isPresent(int arr[][4], int target, int row, int col) {
     return false;
 }
 
+void printSum(int arr[][4], int row, int col) {
+    int sum = 0;
+    for(int row=0; row<3; row++) {
+        for(int col=0; col<4; col++) {
+            sum += arr[row][col];
+        }
+        cout << sum << " ";
+    }
+}
+
 int main() {
     // create 2d array
     int arr[3][4];
-
-    // int arr[3][4] = {{1,11,111,1111}, {2,22,222,2222}, {3,33,333,3333}};
-    
-    // taking input --> row wise input
-    // for(int row=0; row<3; row++) {
-    //     for(int col=0; col<4; col++) {
-    //         cin >> arr[row][col];
-    //     }
-    // }
 
     // taking input --> column wise input
     for(int col=0; col<4; col++) {
@@ -39,7 +41,7 @@ int main() {
         }
         cout << endl;
     }
-
+    
     cout << "Enter Element to Search: " << endl;
     int target;
     cin >> target;
@@ -49,6 +51,8 @@ int main() {
     } else {
         cout << "Element Not Found " << endl;
     }
+    
+    printSum(arr, 3,4);
     
     return 0;
 }
