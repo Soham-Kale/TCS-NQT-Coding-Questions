340457bcd806811dfc5aab5038707fa176285de6
Soham-Kale <sohamkale3014@gmail.com>
2026-04-21 14:26:29 +0530

    Add problem description and implementation for counting Sundays in a given month

---
+++ PYQ/simpleQuestions/sundayCount/index.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    string s;
+    cin >> s;
+
+    int a;
+    cin >> a;
+
+    unordered_map<string, int> m;
+    m["mon"] = 6;
+    m["tue"] = 5;
+    m["wed"] = 4;
+    m["thu"] = 3;
+    m["fri"] = 2;
+    m["sat"] = 1;
+    m["sun"] = 0;
+
+    int ans = 0;
+
+    if(a > m[s]) {
+        ans = 1 + (a - m[s] - 1) / 7;
+    }
+
+    cout << ans;
+    return 0;
+}
