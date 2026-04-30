28032e4e164a18a559fd5b517d61aca73e4d5e21
Soham-Kale <sohamkale3014@gmail.com>
2026-04-30 21:44:36 +0530

    Enhance comments for clarity in removeDuplicate function

--- PYQ/string/removeAllConsecutiveDuplicateCharacters/index.cpp
+++ PYQ/string/removeAllConsecutiveDuplicateCharacters/index.cpp
@@ -3,12 +3,15 @@ using namespace std;
 
 string removeDuplicate(string input) {
     
+    // If the input string is empty, return an empty string
     if(input.empty()) return "";
     string result;
     
+    // Always add the first character to the result
     result += input[0];
     
     for(int i=1; i<input.length(); ++i) {
+        // If the current character is different from the previous one, add it to the result
         if(input[i] != input[i-1]) {
             result += input[i];
         }
