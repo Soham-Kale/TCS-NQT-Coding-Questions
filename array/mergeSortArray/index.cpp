a0342042d9517ba291fabdbc348dff2db23b92db
Soham-Kale <sohamkale3014@gmail.com>
2026-05-03 19:58:46 +0530

    Refactor merge function implementation in Solution class for clarity

--- array/mergeSortArray/index.cpp
+++ array/mergeSortArray/index.cpp
@@ -47,24 +47,24 @@ int main() {
 // Input: nums1 = [1,2,3,0,0,0], m = 3, nums2 = [2,5,6], n = 3
 // Output: [1,2,2,3,5,6]
 
-// class Solution {
-// public:
-//     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-//         int i = m - 1;          // last valid in nums1
-//         int j = n - 1;          // last in nums2
-//         int k = m + n - 1;      // last position in nums1
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        int i = m - 1;          // last valid in nums1
+        int j = n - 1;          // last in nums2
+        int k = m + n - 1;      // last position in nums1
 
-//         while(i >= 0 && j >= 0) {
-//             if(nums1[i] > nums2[j]) {
-//                 nums1[k--] = nums1[i--];
-//             } else {
-//                 nums1[k--] = nums2[j--];
-//             }
-//         }
+        while(i >= 0 && j >= 0) {
+            if(nums1[i] > nums2[j]) {
+                nums1[k--] = nums1[i--];
+            } else {
+                nums1[k--] = nums2[j--];
+            }
+        }
 
-//         // copy remaining nums2
-//         while(j >= 0) {
-//             nums1[k--] = nums2[j--];
-//         }
-//     }
-// };
+        // copy remaining nums2
+        while(j >= 0) {
+            nums1[k--] = nums2[j--];
+        }
+    }
+};
