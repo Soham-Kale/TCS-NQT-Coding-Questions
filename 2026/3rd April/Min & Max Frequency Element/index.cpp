f6d3ba62c670fcc6e982e1afd938f5257c9caa9c
Soham-Kale <sohamkale3014@gmail.com>
2026-05-03 19:58:37 +0530

    Enhance comments for clarity in frequency map creation and min/max frequency tracking

--- 2026/3rd April/Min & Max Frequency Element/index.cpp
+++ 2026/3rd April/Min & Max Frequency Element/index.cpp
@@ -5,16 +5,25 @@ int main() {
     cin >> n; 
     int arr[n]; 
 
+    // Create a frequency map to count the occurrences of each element
     unordered_map<int, int> freq;
+
     for(int i=0; i<n; i++) { 
         cin >> arr[i]; 
+
+        // Increment the frequency count for the current element
         freq[arr[i]]++; 
     } 
     
+    // Initialize variables to track the minimum and maximum frequency and their corresponding elements
     int minFreq = INT_MAX , maxFreq = INT_MIN; 
+
+    // Initialize minElement and maxElement to the first element of the array
     int minElement = arr[0], maxElement = arr[0]; 
 
+    // Iterate through the frequency map to find the minimum and maximum frequency elements
     for(auto it: freq) { 
+        // Update the minimum frequency and corresponding element if a lower frequency is found
         if(it.second < minFreq) { 
             minFreq = it.second; 
             minElement = it.first; 
